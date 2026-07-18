8ffff3f8eb147e3ccb699613090995ef83212f5e
Russ Middleton <me@russmiddleton.com>
2026-07-18 02:11:20 -0400

    feat: export handler option and delta types from the package root

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@ export * from './util/easing';
 export * from './util/browser';
 export * from './util/dom';
 
+export type { HandlerManagerOptions } from './handlers/handlerManager';
+export type { HandlerAxes, HandlerDelta } from './handlers/types';
+export type { ScrollZoomOptions } from './handlers/scrollZoomHandler';
+export type { MousePanOptions } from './handlers/mousePanHandler';
+export type { MouseRotatePitchOptions } from './handlers/mouseRotatePitchHandler';
+export type { TouchMultiOptions } from './handlers/touchMultiHandler';
+export type { KeyboardOptions } from './handlers/keyboardHandler';
+export type { DblclickOptions } from './handlers/dblclickHandler';
+export type { BoxZoomOptions } from './handlers/boxZoomHandler';
+export type { SafariGestureOptions } from './handlers/safariGestureHandler';
+
